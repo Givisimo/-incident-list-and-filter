b49013787c7e353f6a65b436eb389db7bd612593
Denys Plietnikov <givisimo@gmail.com>
2020-11-15 00:42:40 +0200

    custom-modal change

--- src/custom-modal/modal.js
+++ src/custom-modal/modal.js
@@ -2,10 +2,12 @@ import { createCustomElement } from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import view from './view';
 import { actionHandlers } from './actionHandlers';
+import styles from './styles.scss';
 
 createCustomElement('custom-modal', {
   renderer: { type: snabbdom },
   view,
+  styles,
   actionHandlers: actionHandlers,
   properties: {
     modalOpen: { default: '' },
